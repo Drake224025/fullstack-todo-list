72f14e248799454d6db2b3f29220df930797aceb
Drake224025 <sasithamare@gmail.com>
2024-04-24 09:46:47 +0530

    feat(backend): Add todo tasks in seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -5,9 +5,15 @@ db.serialize(() => {
   // Serialize ensures tasks are added sequentially
   db.run("DELETE FROM tasks"); // Optionally clear any existing tasks
 
-  db.run("INSERT INTO tasks (title) VALUES ('Buy groceries')");
-  db.run("INSERT INTO tasks (title) VALUES ('Complete assignment')");
-  db.run("INSERT INTO tasks (title, completed) VALUES ('Walk the dog', TRUE)"); // Example of a completed task
+  db.run("INSERT INTO tasks (title) VALUES ('Got milk?')");
+  db.run("INSERT INTO tasks (title) VALUES ('Squash bugs')");
+  db.run(
+    "INSERT INTO tasks (title, completed) VALUES ('Take a chill pill', TRUE)"
+  ); // Example of a completed task
+  db.run("INSERT INTO tasks (title) VALUES ('Zzz')");
+  db.run("INSERT INTO tasks (title) VALUES ('Java Jolt')");
+  db.run("INSERT INTO tasks (title) VALUES ('Ninja code')");
+  db.run("INSERT INTO tasks (title) VALUES ('Code, eat, sleep')");
 });
 
 console.log("Seed data added to the database");
